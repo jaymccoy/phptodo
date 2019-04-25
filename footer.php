1493a79a87e952feb6451f4c4130c3c8b51c12a5
Jeroen Bouman <jeroen.bouman@sqills.com>
2019-04-25 18:11:36 +0200

    - Added session - Added accordion

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
 
+<div class="footer">
+<!--    --><?php //print_r($_SESSION);?>
+<!--    --><?php //var_dump($item_list);?>
+</div>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php include_once("header.php"); ?>
+<?php include_once 'header.php'; ?>
+<?php
+    $selected_list=$_SESSION['selected_list'];
+    if ($_GET['list'] && '' !== $_GET['list']) {
+        $selected_list=$_GET['list'];
+        $_SESSION['selected_list']=$selected_list;
+    }//end if
+?>
 
     <div class="btn-group">
         <button type="button" class="btn btn-primary">About</button>
@@ -21,14 +28,63 @@
                                 }//end while
                                 //TODO: look into a better way to count the number of items in a file
                                 $item_count=floor(($line_count-2)/6);
-                                echo "<a class=\"dropdown-item\" href=\"#\">$file_name <span class=\"badge badge-primary badge-pill\">$item_count</span></a>";
+                                echo "<a class=\"dropdown-item\" href=\"?list=$file_name\">$file_name <span class=\"badge badge-primary badge-pill\">$item_count</span></a>";
                             }//end if
                         }//end while
                     }//end if
                 ?>
             </div>
         </div>
+<!--        TODO: make menu able to toggle left and right-->
+<!--        <button type="button" class="btn btn-primary">Move right</button>-->
+    </div>
+    <h4>&nbsp;<?php echo $selected_list; ?></h4>
+
+
+    <div id="accordion">
+<?php
+    if ('' !== $selected_list) {
+        $item_list = json_decode(file_get_contents("./lists/$selected_list.json"));
+        foreach ($item_list as $key => $item) {
+            ?>
+            <div class="card">
+                <div class="card-header">
+                    <a class="collapsed card-link" data-toggle="collapse" href="#collapse<?php echo $key; ?>">
+                        <?php echo $item->text; ?>
+                    </a>
+                </div>
+                <div id="collapse<?php echo $key; ?>" class="collapse" data-parent="#accordion">
+                    <div class="card-body">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                </div>
+            </div>
+            <?php
+        }//end foreach
+    }//end if
+?>
+
+
+
+        <div class="card">
+            <div class="card-header">
+                <a class="card-link" data-toggle="collapse" href="#collapseAdd">
+                    Add new item
+                </a>
+            </div>
+            <div id="collapseAdd" class="collapse" data-parent="#accordion">
+                <div class="card-body">
+                    <form class="form-inline" name="add_list_item">
+                        <input type="hidden" name="list" value="<?php echo $selected_list; ?>">
+                        <label for="email2" class="mb-2 mr-sm-2">Item:</label>
+                        <input type="text" class="form-control mb-2 mr-sm-2" id="item_text" placeholder="Enter list item" name="item_text">
+                        <button type="submit" class="btn btn-primary mb-2">Add</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 
 
-<?php include_once("footer.php"); ?>
+
+<?php include_once 'footer.php'; ?>
